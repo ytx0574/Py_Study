9fc7129c83a777c1868cb82231610871297bed5e
Johnson <ytx0573@sina.com>
2018-11-03 10:24:39 +0800

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,11 @@
+# -- coding: utf-8 --
+my_name = "johnson"
+age = 11
+height = 170
+weight = 55.55
+
+print my_name, age, height, weight
+
+print "my_weight = %.2f" % weight, "身高 = %d", height
+
+print "name = %s, age = %d %d" % (my_name, age, 987)
---
+++ ex6.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+x = "There are %d types of people." % 11
+binary = "这是一个python代码文本"
+value = True
+value1 = 55 % 10 == 1
+
+
+print x, binary, value1, value
